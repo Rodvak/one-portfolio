7ae0209406e4e73e4dce7bbe667d5c6170d81963
Alex <av.rodriguez@outlook.com>
2023-03-10 17:07:57 -0800

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,11 +15,11 @@ const About = () => {
         <p className="font-signature text-xl mt-5">
           Hello world! My name is Alejandro but you can call me Alex. I hold a
           Bachelors of International Commerce and Business from EBC Mexico City
-          and have 6+ years of sales, project management and customer service
-          experience in the technology field. During the pandemic I've come to
+          and have 5 years of customer service
+          and 2 in tech Sales. During the pandemic I've come to
           realize that life is very short and that we should live every day to
-          the fullest. It was the moment I decided to reconect with my passion
-          for coding and pursue it as a lifelong career path. I am a proud
+          the fullest. It was the moment I decided to reconect with another of my passions,
+          "coding". I am a proud
           graduate of Coding Dojo's 2022 Software engineering program, where I
           was able to dive into 2000+ programming hours where I was able to
           connect and collaborate with an amazing group of people who also share
@@ -31,8 +31,8 @@ const About = () => {
             outdoors; Going to the beach, hiking, playing sports. My favorite
             hobbies are travelling, learning other languages (I'm now fluent in
             Portuguese!!) and trying new food. My goal to future is to leverage
-            my experience in programming and to inspire the younger
-            Latin-american community to join Tech.
+            my experience in sales and programming to inspire the younger
+            Latin community to join Tech.
           </p>
         </p>
       </div>
